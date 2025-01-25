a16484ed89f21681fdd7ed618995c2bfe03ffb6f
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-01-25 10:20:41 +0530

    Milestone5 Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import {LoginPage} from './Routers.jsx'
+import {LoginPage,SignupPage} from './Routers.jsx'
 import './App.css'
 
 function App() {
@@ -7,6 +7,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage/>} />
+          <Route path="/signup" element={<SignupPage/>} />
+
         </Routes>
       </BrowserRouter>
   );
