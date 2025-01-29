85b72860075f49acc9a7225710a90b17709148eb
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-01-29 11:57:37 +0530

    mileston 8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import {LoginPage,SignupPage} from './Routers.jsx'
-import './App.css'
-
-function App() {
+import { BrowserRouter , Routes , Route} from "react-router-dom";
+import { LoginPage , SignupPage } from "./Routers.jsx";
+import './App.css' ;
+import Home from "./pages/Home.jsx";
+function App (){
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/signup" element={<SignupPage/>} />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<Home />} />
 
-        </Routes>
-      </BrowserRouter>
-  );
+    
+    </Routes>
+    </BrowserRouter>
+  )
 }
-
 export default App;
